7b67bc3095b2e974abf68e2a915a5742f6b8ce56
sunshinebo <917007408@qq.com>
2020-04-09 14:33:27 +0800

    辅助函数

---
+++ bootstrap/helpers.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lizongyang
+ * Date: 2020-04-09
+ * Time: 14:21
+ * 自定义辅助函数存放的地方
+ */
+
+/**
+ * @return string
+ */
+function test_helper()
+{
+    return 'Ok';
+}
