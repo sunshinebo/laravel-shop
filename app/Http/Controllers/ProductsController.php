7f01a3fa25b1cfb9787732d190597b2adae772f1
sunshinebo <917007408@qq.com>
2020-04-17 23:17:43 +0800

    用户界面 - 商品详情页

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -54,4 +54,21 @@ class ProductsController extends Controller
             ],
         ]);
     }
+
+    /**
+     * @param Product $product
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Exception
+     * 页面详情
+     */
+    public function show(Product $product, Request $request)
+    {
+        // 判断商品是否已经上架，如果没有上架则抛出异常。
+        if (!$product->on_sale) {
+            throw new \Exception('商品未上架');
+        }
+
+        return view('products.show', ['product' => $product]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,19 +24,21 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes(['verify' => true]);
 
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
-Route::group(['middleware'=>['auth','verified']],function (){
+Route::group(['middleware' => ['auth', 'verified']], function () {
     //收获地址列表
-    Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
     //新建收获地址
-    Route::get('user_addresses/create','UserAddressesController@creat')->name('user_addresses.create');
+    Route::get('user_addresses/create', 'UserAddressesController@creat')->name('user_addresses.create');
     Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
     //修改收获地址
-    Route::get('user_addresses/{user_address}','UserAddressesController@edit')->name('user_addresses.edit');
+    Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
     //处理修改操作
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     //删除
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
-    //商品列表
-    Route::redirect('/','/products')->name('root');
-    Route::get('products','ProductsController@index')->name('products.index');
 });
+//商品列表访客也能访问
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
+//商品详情访客也能访问
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
