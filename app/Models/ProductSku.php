b9aee8e1ba1fad7edcedd11342ab53499941781a
sunshinebo <917007408@qq.com>
2020-04-18 14:30:21 +0800

    创建订单

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -2,14 +2,47 @@
 
 namespace App\Models;
 
+use App\Exceptions\InternalException;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductSku extends Model
 {
     protected $fillable = ['title', 'description', 'price', 'stock'];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function product()
     {
         return $this->belongsTo(Product::class);
     }
+
+    /**
+     * @param $amount
+     * @return mixed
+     * @throws InternalException
+     */
+    public function decreaseStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('减库存不可小于0');
+        }
+
+        //decrement方法减少避免出现为负值的情况
+        return $this->where('id', $this->id)->where('stock', '>=', $amount)->decrement('stock', $amount);
+    }
+
+    /**
+     * @param $amount
+     * @throws InternalException
+     */
+    public function addStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('加库存不可小于0');
+        }
+        //increment() 方法来保证操作的原子性。加库存
+        $this->increment('stock', $amount);
+    }
+
 }
