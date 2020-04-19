81c9b4c0fb9b1c207510c69ba8d7fe78977d2050
sunshinebo <917007408@qq.com>
2020-04-19 15:05:14 +0800

    集成支付宝

--- routes/web.php
+++ routes/web.php
@@ -57,7 +57,15 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('orders', 'OrdersController@index')->name('orders.index');
     //用户订单详情
     Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
+    Route::get('alipay', function() {
+        return app('alipay')->web([
+            'out_trade_no' => time(),
+            'total_amount' => '1',
+            'subject' => 'test subject - 测试',
+        ]);
+    });
 });
 
 //商品详情访客也能访问
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
